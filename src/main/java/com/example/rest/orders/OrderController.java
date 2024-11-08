5ecea1198080fe38d20d8cd3cb816b128bcb8040
Vyacheslav Markov <slavacor8001@gmail.com>
2024-11-08 16:23:30 +0600

    last steps at links

---
+++ src/main/java/com/example/rest/orders/OrderController.java
@@ -0,0 +1,55 @@
+package com.example.rest.orders;
+
+import org.aspectj.weaver.ast.Or;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class OrderController {
+
+    private final OrderRepository repository;
+    private final OrderModelAssembler assembler;
+
+    public OrderController(OrderRepository repository, OrderModelAssembler assembler) {
+        this.repository = repository;
+        this.assembler = assembler;
+    }
+
+    @GetMapping("/orders")
+    CollectionModel<EntityModel<Order>> all(){
+        List<EntityModel<Order>> orders = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(orders,
+                linkTo(methodOn(OrderController.class).all()).withSelfRel());
+    }
+
+    @GetMapping
+    EntityModel<Order> one(@PathVariable Long id){
+
+        Order order = repository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        return assembler.toModel(order);
+    }
+
+    @PostMapping("/orders")
+    ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order){
+        order.setStatus(Status.IN_PROGRESS);
+        Order newOrder = repository.save(order);
+
+        return ResponseEntity
+                .created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
+                .body(assembler.toModel(newOrder));
+    }
+}
---
+++ src/main/java/com/example/rest/orders/OrderModelAssembler.java
@@ -0,0 +1,24 @@
+package com.example.rest.orders;
+
+import com.example.rest.employees.EmployeeController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class OrderModelAssembler implements RepresentationModelAssembler<Order,EntityModel<Order>> {
+
+    @Override
+    public EntityModel<Order> toModel(Order order) {
+        EntityModel<Order> orderEntityModel = EntityModel.of(order,
+                linkTo(methodOn(EmployeeController.class).one(order.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("order"));
+        if (order.getStatus() == Status.IN_PROGRESS){
+            orderEntityModel.add(linkTo(methodOn(OrderController.class).cancel()))
+        }
+    }
+
+}
