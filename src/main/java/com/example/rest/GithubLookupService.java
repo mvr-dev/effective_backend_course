63a58321936a9e79e017d2df1c9548e084e1e7d3
Vyacheslav Markov <slavacor8001@gmail.com>
2024-11-27 22:19:59 +0600

    created github lookup service

---
+++ src/main/java/com/example/rest/GithubLookupService.java
@@ -0,0 +1,28 @@
+package com.example.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class GithubLookupService {
+    private static final Logger logger = LoggerFactory.getLogger(GithubLookupService.class);
+    private final RestTemplate restTemplate;
+
+    public  GithubLookupService(RestTemplateBuilder restTemplateBuilder){
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Async
+    public CompletableFuture<User> findUser(String user) throws InterruptedException{
+        logger.info("Looking up "+user);
+        String url = String.format("https://api.github.com/users/%s", user);
+        User result = restTemplate.getForObject(url,User.class);
+        return CompletableFuture.completedFuture(result);
+    }
+}
--- src/main/java/com/example/rest/RestApplication.java
+++ src/main/java/com/example/rest/RestApplication.java
@@ -2,12 +2,28 @@ package com.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class RestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+	@Bean
+	public Executor taskExecutor(){
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(5);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GithubLookup -");
+		executor.initialize();
+		return executor;
+	}
 }
